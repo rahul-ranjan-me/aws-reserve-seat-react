9ae5bb826295f83444faca6c891c87742e2d1676
Rahul Ranjan <rahul.smile@gmail.com>
2019-11-17 20:05:02 +0530

    added build folder

---
+++ build/precache-manifest.5a85fb2206527f0f6138e8f97e484b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6a6cd798d7b161576182329dba8245",
+    "url": "/index.html"
+  },
+  {
+    "revision": "647569e4574987f9729f",
+    "url": "/static/css/2.caed53af.chunk.css"
+  },
+  {
+    "revision": "9f68335555a5f4868841",
+    "url": "/static/css/main.4f16040b.chunk.css"
+  },
+  {
+    "revision": "647569e4574987f9729f",
+    "url": "/static/js/2.fe8bd14a.chunk.js"
+  },
+  {
+    "revision": "9f68335555a5f4868841",
+    "url": "/static/js/main.b0bc06c1.chunk.js"
+  },
+  {
+    "revision": "959866cf30fddd10936e",
+    "url": "/static/js/runtime-main.b6728ef1.js"
+  }
+]);
